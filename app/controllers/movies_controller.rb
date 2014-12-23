6a54aa2037643a7745c5bcdc81a857cf9c49e942
Ermand Duro <ermand.duro@gmail.com>
2014-12-23 18:16:26 +0100

    Add searchkick for elasticsearch

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -70,6 +70,15 @@ class MoviesController < ApplicationController
     end
   end
 
+  def search
+    if params[:search].present?
+      @movies = Movie.search(params[:search])
+    else
+      @movies = Movie.all
+    end
+    puts @movies
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_movie
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,6 @@ class Movie < ActiveRecord::Base
 
   has_attached_file :image, styles: { medium: "400x600#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  searchkick
 end
